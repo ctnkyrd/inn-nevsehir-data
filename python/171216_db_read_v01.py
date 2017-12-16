4b12f40b06ac67c1e4aaefc61918db880f0151e4
Arda Çetinkaya <ctnkyrd@gmail.com>
2017-12-16 14:22:47 +0300

    utf sorunu çözüldü sanırsam

--- python/171216_db_read_v01.py
+++ python/171216_db_read_v01.py
@@ -1,9 +1,43 @@
+# -*- coding: utf-8 -*-
+
+
 import psycopg2
+import psycopg2.extensions
+psycopg2.extensions.register_type(psycopg2.extensions.UNICODE)
+psycopg2.extensions.register_type(psycopg2.extensions.UNICODEARRAY)
 
+#==========================================================================================
+#Writer : Arda Çetinkaya
+#Initiation Date : 16-12-2017 13:50
+
+#DB Parameters==============================================================================
+host = 'localhost'
+user = 'postgres'
+password = 'kalman'
+dbName = 'DataCollecitonDB'
+#DB Parameters==============================================================================
+
+
+#ConnString=================================================================================
 try:
-    conn = psycopg2.connect("dbname='DataCollecitonDB' user='postgres' host='localhost' password='kalman'")
+    conn = psycopg2.connect("dbname="+dbName+" user="+user+" host="+host+" password="+password)
+    conn.set_client_encoding('UTF-8')
     print "Connected Succesfully!"
-except:
+except BaseException as Be:
     print "I am unable to connect to the database"
+    print Be.message
+#ConnString=================================================================================
 
 
+
+#TheCode====================================================================================
+try:
+    cur = conn.cursor()
+    cur.execute("""SELECT yapi_avlu_ici_yerlesim from yapidata_son""")
+    rows = cur.fetchall()
+    for row in rows:
+        print row[0]
+except BaseException as Be:
+    print Be.message
+
+print "all completed!"
