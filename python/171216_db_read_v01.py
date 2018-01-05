c960c7ae7f71b9db4faaaca0a0c1607832443541
Arda Çetinkaya <ctnkyrd@gmail.com>
2018-01-05 22:47:59 +0300

    dictionary added,  html report is cancelled!

--- python/171216_db_read_v01.py
+++ python/171216_db_read_v01.py
@@ -25,32 +25,71 @@ try:
     conn.set_client_encoding('UTF-8')
     print "Connected Succesfully!"
 except BaseException as Be:
-    print "I am unable to connect to the database"
+    print "Unable to connect to the database"
     print Be.message
 #ConnString=================================================================================
+
+#Dictionary=================================================================================
+global tableNoDict
+tableNoDict = {}
+tableNoDict[1] = 0
+tableNoDict[2] = 0
+tableNoDict[3] = 0
+tableNoDict[4] = 0
+tableNoDict[5] = 0
+tableNoDict[6] = 0
+tableNoDict[7] = 0
+tableNoDict[8] = 0
+tableNoDict[9] = 1
+tableNoDict[10] = 1
+tableNoDict[11] = 0
+tableNoDict[12] = 1
+tableNoDict[13] = 1
+tableNoDict[14] = 0
+tableNoDict[15] = 1
+tableNoDict[16] = 0
+tableNoDict[17] = 0
+tableNoDict[18] = 1
+tableNoDict[19] = 0
+tableNoDict[20] = 0
+tableNoDict[21] = 0
+tableNoDict[22] = 0
+tableNoDict[23] = 0
+tableNoDict[24] = 0
+tableNoDict[25] = 0
+tableNoDict[26] = 0
+tableNoDict[27] = 0
+tableNoDict[28] = 1
+tableNoDict[29] = 1
+tableNoDict[30] = 1
+tableNoDict[31] = 1
+tableNoDict[32] = 1
+tableNoDict[33] = 1
+tableNoDict[34] = 1
+tableNoDict[35] = 1
+tableNoDict[36] = 1
+tableNoDict[37] = 1
+tableNoDict[38] = 0
+tableNoDict[39] = 0
+tableNoDict[40] = 1
+tableNoDict[41] = 1
+tableNoDict[42] = 1
+tableNoDict[43] = 0
+tableNoDict[44] = 0
+tableNoDict[45] = 0
+tableNoDict[46] = 0
+#Dictionary=================================================================================
 
 
 
 #TheCode====================================================================================
-# try:
-#     cur = conn.cursor()
-#     cur.execute("""SELECT * from yapidata_son""")
-#     rows = cur.fetchall()
-#     for row in rows:
-#         print row[0]
-# except BaseException as Be:
-#     print Be.message
-
-import pdfkit
-
-body = """
-    <html>
-      <head>
-        <meta name="pdfkit-page-size" content="Legal"/>
-        <meta name="pdfkit-orientation" content="Landscape"/>
-      </head>
-      Hello World!
-      </html>
-    """
-
-pdfkit.from_string(body, 'out.pdf') #with --page-size=Legal and --orientation=Landscape
+try:
+    cur = conn.cursor()
+    cur.execute("""SELECT * from yapidata_son""")
+    rows = cur.fetchall()
+    rowCounter = 0
+    for row in rows:
+        print row[0]
+        rowCounter += 1
+except BaseException as Be:
+    print Be.message
