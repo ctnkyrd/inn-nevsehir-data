21638d6567a56b7a7ee77201569a7de3c6edac9b
Arda Çetinkaya <ctnkyrd@gmail.com>
2018-02-05 18:02:23 +0300

    unicode

--- python/180205_create_excel_report.py
+++ python/180205_create_excel_report.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-
+import codecs
 import sys, os, xlsxwriter
 
 workBookLocation = "E:\\"
@@ -14,8 +14,14 @@ worksheet.set_paper(9)
 
 worksheet.set_column('A:A', 20)
 bold = workbook.add_format({'bold': True})
-worksheet.write('A1', 'Hello')
-worksheet.write('A2', 'World', bold)
-worksheet.write(2, 0, 123)
-worksheet.write(3, 0, 123.456)
+
+format = workbook.add_format()
+format.set_bg_color('#808080')
+format.set_font_color('white')
+format.set_bold(True)
+
+
+worksheet.write('A1', u'[Ayvalı]',format)
+
+
 workbook.close()
