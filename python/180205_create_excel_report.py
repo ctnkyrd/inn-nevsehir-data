d61421f6e160c367e4e9ed55894ad04bad18fc10
Arda Çetinkaya <ctnkyrd@gmail.com>
2018-02-06 14:00:08 +0300

    excel formatter

--- python/180205_create_excel_report.py
+++ python/180205_create_excel_report.py
@@ -1,5 +1,4 @@
 # -*- coding: utf-8 -*-
-import codecs
 import sys, os, xlsxwriter
 
 workBookLocation = "E:\\"
@@ -11,17 +10,31 @@ worksheet = workbook.add_worksheet()
 worksheet.set_portrait()
 worksheet.set_page_view()
 worksheet.set_paper(9)
+worksheet.set_margins(left=0.1,right=0.1)
 
-worksheet.set_column('A:A', 20)
-bold = workbook.add_format({'bold': True})
+worksheet.set_column('A:A', 17.8)
+worksheet.set_column('B:B', 2.0)
+worksheet.set_column('C:C', 17.8)
+worksheet.set_column('D:D', 11.26)
+worksheet.set_column('E:E', 0.76)
+worksheet.set_column('F:F', 11.3)
+worksheet.set_column('G:G', 16.6)
+
+
 
 format = workbook.add_format()
 format.set_bg_color('#808080')
 format.set_font_color('white')
 format.set_bold(True)
+format.set_align('center')
+format.set_font_name('Arial Narrow')
+format.set_border(2)
+format.set_font_size(10)
+
 
 
 worksheet.write('A1', u'[Ayvalı]',format)
+worksheet.write('C1', u'ADA/PARSEL',format)
 
 
 workbook.close()
