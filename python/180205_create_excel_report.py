cdf969cad207eef7ae0a4df3b4531141ae8898e7
Arda Çetinkaya <ctnkyrd@gmail.com>
2018-02-07 10:39:28 +0300

    format added

--- python/180205_create_excel_report.py
+++ python/180205_create_excel_report.py
@@ -12,6 +12,7 @@ worksheet.set_page_view()
 worksheet.set_paper(9)
 worksheet.set_margins(left=0.1,right=0.1)
 
+#set column widths
 worksheet.set_column('A:A', 17.8)
 worksheet.set_column('B:B', 2.0)
 worksheet.set_column('C:C', 17.8)
@@ -19,22 +20,51 @@ worksheet.set_column('D:D', 11.26)
 worksheet.set_column('E:E', 0.76)
 worksheet.set_column('F:F', 11.3)
 worksheet.set_column('G:G', 16.6)
+worksheet.set_column('H:H', 8.5)
+
+
+#header1 format_h1
+format_h1 = workbook.add_format()
+format_h1.set_bg_color('#808080')
+format_h1.set_font_color('white')
+format_h1.set_bold(True)
+format_h1.set_align('center')
+format_h1.set_font_name('Arial Narrow')
+format_h1.set_border(2)
+format_h1.set_font_size(10)
+#header2 format_h2
+format_h2 = workbook.add_format()
+format_h2.set_bg_color('#d9d9d9')
+format_h2.set_font_color('black')
+format_h2.set_bold(True)
+format_h2.set_align('center')
+format_h2.set_font_name('Arial Narrow')
+format_h2.set_border(1)
+format_h2.set_font_size(8.5)
+#header3 format_h3
+format_h3 = workbook.add_format()
+format_h3.set_bg_color('#bfbfbf')
+format_h3.set_font_color('black')
+format_h3.set_bold(True)
+format_h3.set_align('center')
+format_h3.set_font_name('Arial Narrow')
+format_h3.set_border(1)
+format_h3.set_font_size(8.5)
+#datacell format_dc
+format_dc = workbook.add_format()
+format_dc.set_bg_color('#bfbfbf')
+format_dc.set_font_color('black')
+format_dc.set_bold(True)
+format_dc.set_align('center')
+format_dc.set_font_name('Arial Narrow')
+format_dc.set_border(1)
+format_dc.set_font_size(8.5)
 
 
 
-format = workbook.add_format()
-format.set_bg_color('#808080')
-format.set_font_color('white')
-format.set_bold(True)
-format.set_align('center')
-format.set_font_name('Arial Narrow')
-format.set_border(2)
-format.set_font_size(10)
 
-
-
-worksheet.write('A1', u'[Ayvalı]',format)
-worksheet.write('C1', u'ADA/PARSEL',format)
+worksheet.write('A1', u'[Ayvalı]',format_h1)
+worksheet.write('C1', u'ADA/PARSEL',format_h1)
 
 
 workbook.close()
